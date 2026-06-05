1816db0bac13bf1616483ffa9550a48e6f9423d3
MireauLijbaert <mireau.lijbaert@student.howest.be>
2026-06-05 19:26:23 +0200

    Setup completed

--- Plugins/GameFeatures/LijbaertMireauZombie/Source/LijbaertMireauZombieRuntime/LijbaertMireauZombieRuntime.Build.cs
+++ Plugins/GameFeatures/LijbaertMireauZombie/Source/LijbaertMireauZombieRuntime/LijbaertMireauZombieRuntime.Build.cs
@@ -38,6 +38,7 @@ public class LijbaertMireauZombieRuntime : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"AIModule",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
